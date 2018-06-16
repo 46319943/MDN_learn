173770c21f371d90733c6dc0c66a3838cd3b2bcd
piaoyang <495384481@qq.com>
2018-06-16 11:36:09 +0800

    完成WEB第一章

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,35 @@
 var myHeading = document.querySelector("h1");
 myHeading.innerHTML = "Hello ! Welcome to PiaoYang !";
+
+var myButton = document.querySelector("button");
+
+function setUserName() {
+    var myName = prompt("输入你的名字！");
+    localStorage.setItem("name", myName);
+    myHeading.innerHTML = "Welcome " + myName + " to Piaoyang!";
+
+}
+
+if(!localStorage.getItem("name")){
+    setUserName();
+}
+else{
+    var storedName = localStorage.getItem("name");
+    myHeading.innerHTML = "Welcome " + storedName + " to Piaoyang!";
+}
+myButton.onclick = function(){
+    setUserName();
+}
+
+var myImage = document.querySelector("img");
+myImage.onclick = function () {
+    var mySrc = myImage.getAttribute("src");
+
+    if (mySrc === "images/icon.png") {
+        myImage.setAttribute("src", "images/icon2.png");
+    }
+    else {
+        myImage.setAttribute("src", "images/icon.png");
+    }
+}
+
