7291e9602986b319e10c6d563120bf4c6e38693a
piaoyang <495384481@qq.com>
2018-07-20 23:10:36 +0800

    study harder

---
+++ scripts/TS-2.js
@@ -0,0 +1,32 @@
+var displayedImage = document.querySelector('.displayed-img');
+var thumbBar = document.querySelector('.thumb-bar');
+var btn = document.querySelector('button');
+var overlay = document.querySelector('.overlay');
+/* Looping through images */
+for (var i = 1; i <= 5; i++) {
+    var newImage = document.createElement('img');
+    newImage.setAttribute('src', 'images/pic' + i + '.jpg');
+    thumbBar.appendChild(newImage);
+    // newImage.addEventListener('click',function(event){
+    //     displayedImage.setAttribute('src',(<HTMLImageElement>event.target).getAttribute('src'));
+    // });
+}
+thumbBar.addEventListener('click', function (event) {
+    console.log(event.target.nodeName);
+    if (event.target && event.target.nodeName === 'IMG') {
+        displayedImage.setAttribute('src', event.target.getAttribute('src'));
+    }
+});
+/* Wiring up the Darken/Lighten button */
+var flag = false;
+btn.addEventListener('click', function () {
+    if (flag) {
+        btn.textContent = 'Lighten';
+        overlay.style.background = 'rgba(0,0,0,0.5)';
+    }
+    else {
+        btn.textContent = 'Darken';
+        overlay.style.background = 'rgba(0,0,0,0)';
+    }
+    flag = !flag;
+});
---
+++ scripts/TS-2.ts
@@ -0,0 +1,36 @@
+var displayedImage = document.querySelector('.displayed-img') as HTMLImageElement;
+var thumbBar = document.querySelector('.thumb-bar');
+
+var btn = document.querySelector('button');
+var overlay:HTMLDivElement = document.querySelector('.overlay') as HTMLDivElement;
+
+/* Looping through images */
+for(var i = 1;i <= 5;i++ ){
+    var newImage = document.createElement('img');
+    newImage.setAttribute('src', 'images/pic' + i + '.jpg');
+    thumbBar.appendChild(newImage);
+
+    // newImage.addEventListener('click',function(event){
+    //     displayedImage.setAttribute('src',(<HTMLImageElement>event.target).getAttribute('src'));
+    // });
+}
+
+thumbBar.addEventListener('click',function(event){
+    console.log((<HTMLElement>event.target).nodeName);
+    if(event.target && (<HTMLElement>event.target).nodeName === 'IMG' ){
+        displayedImage.setAttribute('src',(<HTMLImageElement>event.target).getAttribute('src'));
+    }
+});
+
+/* Wiring up the Darken/Lighten button */
+var flag = false;
+btn.addEventListener('click',function(){
+    if(flag){
+        btn.textContent = 'Lighten';
+        overlay.style.background = 'rgba(0,0,0,0.5)';
+    }else{
+        btn.textContent = 'Darken';
+        overlay.style.background = 'rgba(0,0,0,0)';
+    }
+    flag = !flag;
+});
