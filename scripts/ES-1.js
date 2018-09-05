33cacb8794929914534953e8e484c4d57c5ee5b3
piaoyang <495384481@qq.com>
2018-09-05 20:02:39 +0800

    导出练习

--- scripts/ES-1.js
+++ scripts/ES-1.js
@@ -1,5 +1,10 @@
 // 对模块进行加载，相当于执行了那个模块中的内容
 // import './ES-2.js';
 
-// 接口转发
+// 接口转发，相当于先import再export，加载了目标模块
 export * from './ES-2.js';
+
+import * as es from './ES-2.js';
+// import a from './ES-2.js';
+// 默认导出实际上是导出了一个叫作default的变量
+console.log(es.default);
--- scripts/ES-2.js
+++ scripts/ES-2.js
@@ -1,3 +1,4 @@
-let a = 10;
-export {a};
+
+export default a = 10;
+export let b = 20;
 console.log('模块进行了加载');
